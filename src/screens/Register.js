139e57c7498e8ffc584e579e6b4d92bb893923d7
mcittanti <mcittanti@udesa.edu.ar>
2026-06-05 10:29:47 -0300

    funcionalidad register

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import {Text, View, TextInput, Pressable, StyleSheet} from "react-native";
-import {db, auth} from '../firebase/config';
+import { Text, View, TextInput, Pressable, StyleSheet } from "react-native";
+import { db, auth } from '../firebase/config';
 
 function Register(props) {
 
@@ -12,53 +12,55 @@ function Register(props) {
 
     function onSubmit() {
         console.log('Register', { email, username });
-        
+
         auth.createUserWithEmailAndPassword(email, password)
-            .then (response => {
-                db.collection('users').add({
-                    owner:auth.currentUser.email,
+            .then(response => {
+                return db.collection('users').add({
+                    owner: auth.currentUser.email,
                     user: username,
                     email: email,
                     createdAt: Date.now(),
-                })
-
-                setRegister(true)
+                });
+            })
+            .then(() => {          
+                setRegister(true);
                 if (props && props.navigation) props.navigation.navigate('Login');
             })
-            .catch (error => {
-                setRegisterError('Fallo en el registro')
-            })
+            .catch(error => {
+                console.log(error);
+                setRegisterError('Fallo en el registro');
+            });
     }
 
     return (
         <View>
             <Text>Register</Text>
-            <TextInput 
+            <TextInput
                 keyboardType='email-address'
                 placeholder='email...'
-                onChangeText={ text => setEmail(text)}
+                onChangeText={text => setEmail(text)}
                 value={email} />
 
-            <TextInput 
+            <TextInput
                 keyboardType='default'
                 placeholder='username...'
-                onChangeText={ text => setUsername(text)}
+                onChangeText={text => setUsername(text)}
                 value={username} />
 
-            <TextInput 
+            <TextInput
                 keyboardType='default'
                 placeholder='password...'
                 secureTextEntry={true}
-                onChangeText={ text => setPassword(text)}
-                value={password}/>
-            <Pressable  onPress={() => onSubmit()}>
+                onChangeText={text => setPassword(text)}
+                value={password} />
+            <Pressable onPress={() => onSubmit()}>
                 <Text>Register</Text>
             </Pressable>
 
-        <Pressable
-            onPress={() => props.navigation && props.navigation.navigate('Login')}>
-            <Text>Already have an account? Log In </Text>
-        </Pressable>
+            <Pressable
+                onPress={() => props.navigation && props.navigation.navigate('Login')}>
+                <Text>Already have an account? Log In </Text>
+            </Pressable>
         </View>
     )
 }
